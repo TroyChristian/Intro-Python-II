d2cf3f243346cc63ae6f1e792b0b9176bcbfc55f
Troy Christian <apostasy8@gmail.com>
2020-05-06 23:36:40 -0400

    mvp day 1

--- src/room.py
+++ src/room.py
@@ -1,2 +1,16 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+class Room():
+    def __init__(self, name:str, description:str):
+        self.name = name
+        self.description = description
+        self.n_to = None
+        self.s_to = None 
+        self.e_to = None
+        self.w_to = None
+
+    def get_room_in_direction(self, direction):
+        if hasattr(self, f"{direction}_to"):
+            return getattr(self, f'{direction}_to')
+        return None 
+    
